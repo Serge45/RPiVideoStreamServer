9ad1d34f1e8109878e50fa194555130dd0ce8177
Serge45 <Serge45497@gmail.com>
2015-01-27 18:39:21 +0800

    Added simple css file.

--- servo_camera.py
+++ servo_camera.py
@@ -5,7 +5,17 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 
+import platform
+
 global_cam = None
+
+if platform.system() == 'Windows':
+    from VideoCapture import Device
+    global_cam = Device(0)
+
+
+def is_windows():
+    return (platform.system() == 'Windows')
 
 def init_camera():
     pygame.init()
@@ -15,9 +25,13 @@ def get_cam():
     global global_cam
 
     if global_cam == None:
-        init_camera()
-        global_cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (320, 240))
-        global_cam.start()
+        if is_windows():
+            global_cam = Device()
+        else:
+            init_camera()
+            global_cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (320, 240))
+            global_cam.start()
+
     return global_cam
 
 def capture(path="./cap.jpg"):
@@ -28,11 +42,17 @@ def capture(path="./cap.jpg"):
 
 def capture_to_memory():
     cam = get_cam()
-    img = cam.get_image()
-    img_str = pygame.image.tostring(img, 'RGB')
-    pil_img = Image.fromstring('RGB', (320, 240), img_str)
     data = StringIO()
-    pil_img.save(data, 'JPEG')
+
+    if is_windows():
+        img = cam.getImage(timestamp=1)
+        img.save(data, 'JPEG')
+    else:
+        img = cam.get_image()
+        img_str = pygame.image.tostring(img, 'RGB')
+        pil_img = Image.fromstring('RGB', (320, 240), img_str)
+        pil_img.save(data, 'JPEG')
+
     return data.getvalue()
 
 if __name__ == '__main__':
--- servo_client.py
+++ servo_client.py
@@ -1,80 +1,49 @@
-import pigpio
-from flask import Flask, render_template, request
-from flask import Response, jsonify
 import servo_camera
-app = Flask(__name__, static_folder="static")
+import platform
+
+if platform.system() != 'Windows':
+    import pigpio
 
 pi = None
 servo = 18
 pulsewidth = 1500
 
-@app.route("/")
-def index():
-    return render_template("index.html", 
-                           slider_value=pulsewidth,
-                           servo_deg=180)
+def is_pi():
+    return (platform.system() != 'Windows')
 
-@app.route("/", methods=['POST'])
-def move_servo():
+def pulsewidth_to_deg(pw):
+    return ((pw - 500.0) * 180.0) / 2000.0
 
-    if request.method == 'POST':
-        p = int(request.form['slider'])
+def move_servo(p):
+    global pi
+    global servo
 
-        if p < 500:
-            p = 500
-        elif p > 2500:
-            p = 2500
-
-        global pi
-        global servo
-        pi.set_servo_pulsewidth(servo, p)
-        d = ((p - 500.0) * 360.0) / 2000.0
-
-    return render_template("index.html", 
-                           slider_value=p,
-                           servo_deg=d)
-
-@app.route("/_move_servo")
-def move_servo_ajax():
-    print 'Move servo'
-    p = request.args.get("pulsewidth", 1500, type=int)
+    if pi == None:
+        pi = init_pigpio(servo)
 
     if p < 500:
         p = 500
     elif p > 2500:
         p = 2500
 
-    global pi
-    global servo
-    pi.set_servo_pulsewidth(servo, p)
-    deg = ((p - 500.0) * 360.0) / 2000.0
-    return jsonify(result=deg)
+    if is_pi():
+        global pi
+        global servo
+        pi.set_servo_pulsewidth(servo, p)
 
-def get_camera_frame():
-    while True:
-        #frame = servo_camera.capture('./static/cap.jpg')
-        frame = servo_camera.capture_to_memory()
-        yield (b'--frame\r\n' +
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    d = pulsewidth_to_deg(p)
 
-@app.route("/video_feed")
-def video_feed():
-    return Response(get_camera_frame(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return p, d
 
 def init_pigpio(addr):
     global pi
     global pulsewidth
     global servo
     servo = addr
-    pi = pigpio.pi()
-    pi.set_mode(addr, pigpio.OUTPUT)
-    pi.set_servo_pulsewidth(servo, pulsewidth)
-    return pi
-    
 
-if __name__ == '__main__':
-    global servo
-    init_pigpio(servo)
-    app.run(threaded=True, host='0.0.0.0', debug=True)
+    if is_pi():
+        pi = pigpio.pi()
+        pi.set_mode(addr, pigpio.OUTPUT)
+        pi.set_servo_pulsewidth(servo, pulsewidth)
+        
+    return pi
